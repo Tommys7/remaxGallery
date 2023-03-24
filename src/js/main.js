bc88f49c13799c5d6a4a6587e07c35b01db51f9e
tommys7 <tomas.hojgr23@gmail.com>
2023-03-24 11:31:39 +0100

    remax gallery

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function(){
+    new WOW().init();
+
+    $(document).on("scroll", function(){
+        if
+            ($(document).scrollTop() > 50){
+                $(".header").addClass("shrink");
+            }
+        else
+            {
+                $(".header").removeClass("shrink");
+            }
+    })
+
+    $(document).on("scroll", function(){
+        if
+            ($(document).scrollTop() > 50){
+                $(".header-menu__logo").addClass("shrink-logo");
+            }
+        else
+            {
+                $(".header-menu__logo").removeClass("shrink-logo");
+            }
+    })
+
+
+    
+
+
+});
